46cb705d83be287477c929f7d6ee299400ffbc70
Wing Chau <wing@devtography.com>
2021-09-16 23:45:19 +0800

    Group test cases
    - Grouped test cases for functions `attach` & `detach` into a `describe` block.

--- src/lib/odm.spec.ts
+++ src/lib/odm.spec.ts
@@ -7,23 +7,25 @@ const dynamoDBConfig: DynamoDBConfig = {
   schema: new Map(),
 };
 
-it('should return true if ODM instance attached successfully', async () => {
-  const odm = new TyODM(dynamoDBConfig);
-  await odm.attach();
-
-  expect(odm.attached).toBeTruthy();
-});
-
-it('should return true for ODM instance detached successfully', async () => {
-  const odm = new TyODM(dynamoDBConfig);
-  await odm.attach();
-
-  expect(await odm.detach()).toBeTruthy();
-});
-
-it('should return false on attempt to detach an non-attached ODM instance',
-  async () => {
+describe('function `attach` & `detach`', () => {
+  it('should return true if ODM instance attached successfully', async () => {
     const odm = new TyODM(dynamoDBConfig);
+    await odm.attach();
 
-    expect(await odm.detach()).toBeFalsy();
+    expect(odm.attached).toBeTruthy();
   });
+
+  it('should return true for ODM instance detached successfully', async () => {
+    const odm = new TyODM(dynamoDBConfig);
+    await odm.attach();
+
+    expect(await odm.detach()).toBeTruthy();
+  });
+
+  it('should return false on attempt to detach an non-attached ODM instance',
+    async () => {
+      const odm = new TyODM(dynamoDBConfig);
+
+      expect(await odm.detach()).toBeFalsy();
+    });
+});
