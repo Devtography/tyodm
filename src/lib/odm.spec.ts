ffb7d12877170a2a1fb9790bbede9c51dd93fd52
Wing Chau <wing@devtography.com>
2021-09-17 12:54:38 +0800

    DB setup for test cases

--- src/lib/odm.spec.ts
+++ src/lib/odm.spec.ts
@@ -1,10 +1,20 @@
+import {
+  CreateTableCommand,
+  DeleteTableCommand,
+  DynamoDBClient,
+} from '@aws-sdk/client-dynamodb';
+import { MockObj } from '../test-utils/mock-object';
 import { DynamoDBConfig } from './config';
+import { attachDynamoDBClient, detachDynamoDBClient } from './connection';
 import { TyODM } from './odm';
 
 const dynamoDBConfig: DynamoDBConfig = {
   region: 'us-west-1',
+  endpoint: 'http://localhost:8000',
   table: 'default',
-  schema: new Map(),
+  schema: new Map([
+    [MockObj.name, MockObj.SCHEMA],
+  ]),
 };
 
 describe('function `attach` & `detach`', () => {
@@ -28,4 +38,93 @@ describe('function `attach` & `detach`', () => {
 
       expect(await odm.detach()).toBeFalsy();
     });
+});
+
+describe('functions for data retrieve from database', () => {
+  let odm: TyODM;
+
+  let directAccessODM: TyODM;
+  let client: DynamoDBClient;
+
+  beforeAll(async () => {
+    // Setup shared ODM instance.
+    odm = new TyODM(dynamoDBConfig);
+    await odm.attach();
+
+    // Setup DynamoDB client for direct database access.
+    directAccessODM = new TyODM(dynamoDBConfig);
+    client = attachDynamoDBClient(directAccessODM);
+
+    // Create table.
+    await client.send(new CreateTableCommand({
+      TableName: dynamoDBConfig.table,
+      KeySchema: [
+        { AttributeName: 'pk', KeyType: 'HASH' },
+        { AttributeName: 'sk', KeyType: 'RANGE' },
+      ],
+      AttributeDefinitions: [
+        { AttributeName: 'pk', AttributeType: 'S' },
+        { AttributeName: 'sk', AttributeType: 'S' },
+      ],
+      ProvisionedThroughput: { ReadCapacityUnits: 5, WriteCapacityUnits: 5 },
+    }));
+
+    // Put records.
+    await client.send(new TransactWriteItemsCommand({
+      TransactItems: [
+        {
+          Put: {
+            Item: {
+              pk: { S: `${MockObj.name}#${objId}` },
+              sk: { S: 'meta' },
+              objName: { S: 'mock' },
+              objRank: { N: '1' },
+            },
+            TableName: dynamoDBConfig.table,
+          },
+        },
+        {
+          Put: {
+            Item: {
+              pk: { S: `${MockObj.name}#${objId}` },
+              sk: { S: 'row1' },
+              subObj: { M: { prop1: { SS: ['1', '2'] } } },
+            },
+            TableName: dynamoDBConfig.table,
+          },
+        },
+        {
+          Put: {
+            Item: {
+              pk: { S: `${MockObj.name}#${objId}` },
+              sk: { S: `collection#${colId1}` },
+              collectionId: { S: colId1 },
+              sampleSet: { NS: ['1', '2'] },
+            },
+            TableName: dynamoDBConfig.table,
+          },
+        },
+        {
+          Put: {
+            Item: {
+              pk: { S: `${MockObj.name}#${objId}` },
+              sk: { S: `collection#${colId2}` },
+              collectionId: { S: colId2 },
+              sampleSet: { NS: ['1', '2'] },
+            },
+            TableName: dynamoDBConfig.table,
+          },
+        },
+      ],
+    }));
+  });
+
+  afterAll(async () => {
+    await client.send(new DeleteTableCommand({
+      TableName: dynamoDBConfig.table,
+    }));
+    detachDynamoDBClient(directAccessODM);
+
+    await odm.detach();
+  });
 });
