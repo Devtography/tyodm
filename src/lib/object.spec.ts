74e96140e82fa3f15b39cdb3325e3a607bed7838
Wing Chau <wing@devtography.com>
2021-09-18 22:59:33 +0800

    Reuse `MockObj`

--- src/lib/object.spec.ts
+++ src/lib/object.spec.ts
@@ -1,30 +1,7 @@
-import { Obj } from './object';
-import { Schema } from './schema';
+import { MockObj } from '../test-utils/mock-object';
 
-class MockObj extends Obj {
-  static SCHEMA: Schema = {
-    name: 'MockObj',
-    identifier: 'ulid',
-    props: {
-      meta: {
-        type: 'single',
-        attr: {
-          name: 'string',
-        },
-      },
-    },
-  };
+it('should returns the value of identifier specified in schema', () => {
+  const obj = new MockObj();
 
-  ulid = 'ulid';
-
-  objectSchema(): Schema {
-    return MockObj.SCHEMA;
-  }
-}
-
-it('`objectId` should returns the value of identifier specified in schema',
-  () => {
-    const obj = new MockObj();
-
-    expect(obj.objectId).toEqual(obj.ulid);
-  });
+  expect(obj.objectId).toEqual(obj.ulid);
+});
